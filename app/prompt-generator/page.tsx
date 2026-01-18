e4e9c892e59c0e5a72f0ee86b88ad4f19177adbe
Elvis Silva <elviscarlossouza@gmail.com>
2026-01-18 09:04:10 -0300

    update /prompt-generator/page.tsx

--- app/prompt-generator/page.tsx
+++ app/prompt-generator/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState } from 'react';
 import { Box, Container, Typography } from '@mui/material';
 import CategorySelector from '@/app/components/CategorySelector';
